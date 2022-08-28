02846d793f5f3f0383891ffc25ecdd90f72db6f7
Elena <bellenaelena@gmail.com>
2022-08-28 16:15:15 +0300

    добавлено решение задачи 013

---
+++ Задача013/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write ("Enter number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if ((number >= 100) && (number < 1000))
+{
+int lastnumber = number % 10;
+Console.WriteLine ("Last number: " + lastnumber);
+}
+else if (number > 1000)
+{
+    Console.WriteLine ("Bad number");
+}
+
+else if (number < 100)
+{
+    Console.WriteLine ("Last number no");
+}
+
