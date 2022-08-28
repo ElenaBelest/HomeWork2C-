52368968c47abcdc8d95ddb4c535c1e08f30decf
Elena <bellenaelena@gmail.com>
2022-08-28 16:41:16 +0300

    добавлено решение задачи со звездочкой

---
+++ ЗадачаЗвездочка/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Enter number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if ((number % 7 == 0) && (number % 23 == 0))
+{
+    Console.WriteLine ("Number is a multiple of 7 and 23!");
+}
+else
+{
+    Console.WriteLine ("Number is NOT a multiple of 7 and 23");
+}
