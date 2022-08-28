7bdeaeaa44a939acac551add5727188cbfa50e99
Elena <bellenaelena@gmail.com>
2022-08-28 15:48:45 +0300

    добавлено решение задачи 010

---
+++ Задача010/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Enter number: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if ((number >= 100) && (number < 1000))
+{
+int secondnumber = (number % 100) / 10;
+Console.WriteLine ("Second number: " + secondnumber);
+}
+else 
+{
+    Console.WriteLine ("Bad number");
+}
